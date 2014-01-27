1989f30f1d225ba229123ab78b40b062a2802a39
Genghis Chau <gchau@mit.edu>
2014-01-27 01:25:17 -0500

    Can now edit user image

--- foodbook/static/js/profile.js
+++ foodbook/static/js/profile.js
@@ -1,14 +1,11 @@
-var resizeProfileContent = function() {
-	// Resize half circle
-	var profileContentBackground = document.getElementById('profile-content-background');
-	var profileContent = document.getElementById('profile-half-circle');
-	var width = profileContentBackground.offsetWidth;
-	var margin = (width - 100) / 2;
-	profileContent.style.marginLeft = margin;
-	profileContent.style.marginRight = margin;
-	profileContent.style.display = 'block';
-}
+
 
 window.onload = function(event) {
-	resizeProfileContent();
+	$('#profile-image-edit').click(function(){
+		$('#id_image').click();
+	});
+	$('#id_image').change(function(){
+		document.getElementById('profile-pic-form').submit();
+	})
+	resizeImage(150, document.getElementById('profile-image-img'));
 }
