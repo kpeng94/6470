e7bb08a273e57db0ac08cf4c1a7d48c672d855d7
Kevin Peng <kpeng94@mit.edu>
2014-01-27 16:51:29 -0500

    tabs for view

--- foodbook/static/js/view.js
+++ foodbook/static/js/view.js
@@ -2,3 +2,19 @@ var toggleCheckbox = function() {
 	var dietCB = document.getElementById('diet-checkmark');
 	toggleClass(dietCB, 'enabled');
 }
+
+var viewAll = function() {
+	var selectedDiv = document.getElementsByClassName('selected')[0];
+	if (selectedDiv.id == 'vr-mine') {
+		removeClass(selectedDiv, 'selected');
+		addClass(document.getElementById('vr-all'), 'selected');
+	}
+}
+
+var viewMine = function() {
+	var selectedDiv = document.getElementsByClassName('selected')[0];
+	if (selectedDiv.id == 'vr-all') {
+		removeClass(selectedDiv, 'selected');
+		addClass(document.getElementById('vr-mine'), 'selected');
+	}
+}
